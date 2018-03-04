b8cf39996fdadc03b43ff78ff030d68380031312
Martin Haupt <martin23haupt@gmail.com>
2018-03-04 14:32:36 +0100

    deleting old files... promise not working

--- index.js
+++ index.js
@@ -2,20 +2,47 @@ const fs = require('fs');
 const svgexport = require('svgexport');
 const imagemin = require('imagemin');
 const imageminPngquant = require('imagemin-pngquant');
+const del = require('delete');
 const folder = '../_loga/';
-
-const files = fs.readdirSync(folder);
 
 const svg2png = svg =>
   svgexport.render({
-    input: [folder + svg, '512:512', 'pad', '2%'],
+    input: [folder + svg, '512:512', 'pad', '80%'],
     output: [folder + 'temp/' + svg.replace('svg', 'png')]
   });
 
-files.forEach(svg => svg2png(svg));
+const makePngs = () => {
+  const files = fs.readdirSync(folder);
 
-imagemin([folder + 'temp/*.png'], folder + 'png_final', {
-  use: [imageminPngquant()]
-}).then(() => {
-  console.log('Images optimized');
-});
+  // TODO: make this work... it doesn't now
+  return new Promise((resolve, reject) => {
+    if (files.length > 0) {
+      files.forEach(svg => svg2png(svg));
+      resolve('converted');
+    } else {
+      reject('no files');
+    }
+  });
+};
+
+const optimize = () => {
+  imagemin([folder + 'temp/*.png'], folder + 'png_final', {
+    use: [imageminPngquant()]
+  });
+};
+
+const execute = () => {
+  del
+    .promise([folder + 'temp', folder + 'png_final'], {
+      force: true
+    })
+    .then(() => console.log('deleted old files'))
+    .then(() => makePngs())
+    .then(() => optimize())
+    .then(() => {
+      console.log('Done!');
+    })
+    .catch(e => console.error(e));
+};
+
+execute();
